2e89516a8ae890e2487d2eb4a458b6bf8bbeb491
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2021-04-10 13:21:00 +0200

    Rearrange method order.

--- src/Geohash.SpatialIndex.Relations/IGeohashSpatialIndexRelations.cs
+++ src/Geohash.SpatialIndex.Relations/IGeohashSpatialIndexRelations.cs
@@ -7,15 +7,16 @@ namespace Geohash.SpatialIndex.Relations
 {
 	public interface IGeohashSpatialIndexRelations<T>
 	{
-		IEnumerable<GeohashIndexEntry<T>> KNN(int k, Geometry geom, Func<GeohashIndexEntry<T>, double> distanceMetric, T exclude = default);
 		IEnumerable<GeohashIndexEntry<T>> STContains(Geometry geom, T exclude = default);
 		IEnumerable<GeohashIndexEntry<T>> STContainsProperly(Geometry geom, T exclude = default);
 		IEnumerable<GeohashIndexEntry<T>> STCrosses(Geometry geom, T exclude = default);
 		IEnumerable<GeohashIndexEntry<T>> STEquals(Geometry geom, T exclude = default);
 		IEnumerable<GeohashIndexEntry<T>> STIntersects(Geometry geom, T exclude = default);
-		GeohashIndexEntry<T> STNearestNeighbour(Geometry geom, Func<GeohashIndexEntry<T>, double> distanceMetric, T exclude = default);
 		IEnumerable<GeohashIndexEntry<T>> STOverlaps(Geometry geom, T exclude = default);
 		IEnumerable<GeohashIndexEntry<T>> STTouches(Geometry geom, T exclude = default);
 		IEnumerable<GeohashIndexEntry<T>> STWithin(Geometry geom, T exclude = default);
+		GeohashIndexEntry<T> STNearestNeighbour(Geometry geom, Func<GeohashIndexEntry<T>, double> distanceMetric, T exclude = default);
+		IEnumerable<GeohashIndexEntry<T>> KNN(int k, Geometry geom, Func<GeohashIndexEntry<T>, double> distanceMetric, T exclude = default);
+
 	}
 }
