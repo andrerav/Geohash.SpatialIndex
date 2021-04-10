186f1d4bbbb15d8d92810fc7801ad91699094eff
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2021-04-10 14:06:46 +0200

    Add license expression, update Authors, add link to release notes, bump version number. Debug symbols now included when building packages.

--- src/Pack.bat
+++ src/Pack.bat
@@ -1,3 +1,3 @@
 dotnet restore
 REM dotnet msbuild /t:build /p:Configuration=Release /p:GeneratePackageOnBuild=True
-dotnet pack -c Release -o Packages
+dotnet pack -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -o Packages
